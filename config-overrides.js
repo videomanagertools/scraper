3d2688d97c8a51b64750ddde1049bc7dd47356b2
Dec-F <dec_fan@icloud.com>
2018-04-11 17:55:49 +0800

    脚手架 搭建完成

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,5 @@
-const {injectBabelPlugin} = require('react-app-rewired');
-
+const {injectBabelPlugin, compose} = require('react-app-rewired');
+const rewireLess = require("react-app-rewire-less-modules");
 module.exports = function override(config, env) {
     config = injectBabelPlugin([
         'import', {
@@ -8,5 +8,13 @@ module.exports = function override(config, env) {
             style: 'css'
         }
     ], config);
-    return config;
+    const rewires = compose(rewireLess.withLoaderOptions({
+        modifyVars: {
+            // "@primary-color": "#DB7093",
+            // "@info-color": "#DB7093",
+            // "@layout-body-background": "#fff"
+        }
+    }));
+
+    return rewires(config, env);
 };
--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.less';
 import {Button} from 'antd';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h1 className="App-title">Welcome to React</h1>
+      <div className={styles.App}>
+        <header className={styles["App-header"]}>
+          <img src={logo} className={styles["App-logo"]} alt="logo"/>
+          <h1 className={styles["App-title"]}>Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit
