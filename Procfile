bb9fb440f2fe81aa048cce11c82f69c32ed0bdb0
Dec-F <dec_fan@icloud.com>
2019-02-12 18:35:43 +0800

    reset

--- Procfile
+++
@@ -1,2 +0,0 @@
-react: npm start
-electron: nodemon --watch src/electron_main --watch package.json --exec node src/electron-wait-react 
--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './client/index.css';
-import App from './client/App';
-import registerServiceWorker from './client/registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-registerServiceWorker();
