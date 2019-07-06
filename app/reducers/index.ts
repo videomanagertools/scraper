3dcf057f209fae6fc40bee169ac4da0009ba6861
Dec-F <dec_fan@icloud.com>
2019-07-06 19:23:42 +0800

    clear demo

--- app/reducers/index.ts
+++ app/reducers/index.ts
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
-import counter from './counter';
 import fileViewer from './fileViewer';
-
 
 export default function createRootReducer() {
   return combineReducers({
-    counter, fileViewer
+    fileViewer
   });
 }
