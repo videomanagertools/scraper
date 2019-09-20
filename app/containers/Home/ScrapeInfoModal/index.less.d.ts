e42c1d7490cb8ab364ff44ba2eab99cca84303f4
Dec-F <dec_fan@icloud.com>
2019-09-20 18:16:57 +0800

    chore:

--- app/containers/Home/ScrapeInfoModal/index.less.d.ts
+++ app/containers/Home/ScrapeInfoModal/index.less.d.ts
@@ -5,6 +5,7 @@ export const media_title: string;
 export const media_info: string;
 export const info_item: string;
 export const info_label: string;
+export const info_text: string;
 export const uniqueid: string;
 export const genre: string;
 export const actor: string;
